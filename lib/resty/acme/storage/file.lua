91a0623069e44389e7a8085cfba281151c34313f
Danila Balagansky <dbalagansky@me.com>
2020-03-04 14:51:31 +0300

    Fully implement file storage backend

--- lib/resty/acme/storage/file.lua
+++ lib/resty/acme/storage/file.lua
@@ -1,3 +1,5 @@
+local lfs = require('lfs_ffi')
+
 local _M = {}
 local mt = {__index = _M}
 
@@ -15,6 +17,10 @@ function _M.new(conf)
   return self
 end
 
+local function startswith(s, p)
+  return string.sub(s or "", 1, string.len(p)) == p
+end
+
 local function regulate_filename(dir, s)
   -- TODO: not windows friendly
   return dir .. "/" .. ngx.encode_base64(s)
@@ -29,8 +35,53 @@ local function exists(f)
   return err == nil
 end
 
+local function split_ttl(s)
+  local _, _, ttl, value = string.find(s, "(%d+):(.+)")
+
+  return tonumber(ttl), value
+end
+
+local function stat(f)
+  local attrs, err = lfs.attributes(f)
+  if err then
+    return 0
+  else
+    return attrs["modification"]
+  end
+  -- return tonumber(last_modified) or 0
+end
+
+local function check_expiration(f)
+  local file, err = io.open(f, "rb")
+  if err then
+    return
+  end
+
+  local output, err = file:read("*a")
+  file:close()
+
+  if err then
+    return
+  end
+
+  local ttl, value = split_ttl(output)
+
+  if ttl == 0 then
+    return value
+  else
+    if os.difftime(os.time(), stat(f) + ttl) >= 0 then
+      os.remove(f)
+    else
+      return value
+    end
+  end
+end
+
 function _M:add(k, v, ttl)
   local f = regulate_filename(self.dir, k)
+
+  check_expiration(f)
+
   if exists(f) then
     return "exists"
   end
@@ -38,15 +89,23 @@ function _M:add(k, v, ttl)
 end
 
 function _M:set(k, v, ttl)
-  if ttl then
-    return "nyi"
-  end
   local f = regulate_filename(self.dir, k)
+
+  check_expiration(f)
+
+  if ttl then
+    if ttl > 0 and ttl < 1 then
+      ttl = 1
+    end
+  else
+    ttl = 0
+  end
+
   local file, err = io.open(f, "wb")
   if err then
     return err
   end
-  local _, err = file:write(v)
+  local _, err = file:write(ttl .. ":" .. v)
   if err then
     return err
   end
@@ -66,22 +125,25 @@ end
 
 function _M:get(k)
   local f = regulate_filename(self.dir, k)
-  local file, err = io.open(f, "rb")
-  if err then
-    ngx.log(ngx.INFO, "can't read file: ", err)
-    -- TODO: return nil, nil if not found
+
+  local value = check_expiration(f)
+  if value then
+    return value, nil
+  else
     return nil, nil
   end
-  local output, err = file:read("*a")
-  if err then
-    return nil, err
-  end
-  file:close()
-  return output, nil
 end
 
 function _M:list(prefix)
-  return {}, "nyi"
+  local files = {}
+
+  for file in lfs.dir(self.dir) do
+    file = ngx.decode_base64(file)
+    if startswith(file, prefix) then
+      table.insert(files, file)
+    end
+  end
+  return files
 end
 
 return _M
